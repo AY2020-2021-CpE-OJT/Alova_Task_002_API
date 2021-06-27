53e3a1d863e34093e0e44ba758e3c2fd523ea2fd
AmadeoCrisAlova <alovaamadeocris@gmail.com>
2021-06-27 23:30:50 +0800

    adds comment on models/Rhyme.js

--- models/Rhyme.js
+++ models/Rhyme.js
@@ -1,11 +1,11 @@
 //Connect to database
 const mongoose = require('mongoose');
 
-//
+//Hello
 const rhymeSchema = mongoose.Schema({
     rhymeScheme: String,
     rapper: String
 });
 
 //
-module.exports = mongoose.model('rhymeScheme', rhymeSchema);
+module.exports = mongoose.model('rhyme scheme', rhymeSchema);
